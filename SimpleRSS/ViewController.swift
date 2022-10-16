c65263aa709aa341a93c254cba9fb7f154023771
Shuntaro30 <shuntar30@gmail.com>
2022-10-16 14:55:28 +0900

    Initial Commit

---
+++ SimpleRSS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleRSS
+//
+//  Created by shuntaro on 2022/10/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
